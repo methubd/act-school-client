0c99f4aed3756136f69a4c2573f6d68dcc68892a
T R Methu <fmethubd@gmail.com>
2023-06-07 13:51:14 +0600

    routes setup done for user interface

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,9 +1,41 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
+import Home from "../Pages/Home/Home/Home";
+import Instructors from "../Pages/Instructors/Instructors";
+import Classes from "../Pages/Classes/Classes";
+import Dashboard from "../Pages/Dashboard/Dashboard";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
 
 export const router = createBrowserRouter ([
     {
         path: '/',
-        element: <Main></Main>
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/instructors',
+                element: <Instructors></Instructors>
+            },
+            {
+                path: '/classes',
+                element: <Classes></Classes>
+            },
+            {
+                path: '/dashboard',
+                element: <Dashboard></Dashboard>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            }
+        ]
     }
 ])
